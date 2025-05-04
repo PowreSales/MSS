64ac614e2ee97e30e96715bf3e4d253b9876f589
PowreSales <ezra.seidu74@gmail.com>
2025-05-04 22:51:28 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'medicine-sales-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/app.js',
+  '/MSS/index.html',
+  '/MSS/app.js',
   'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js',
   'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
   'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
