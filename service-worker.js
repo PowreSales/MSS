6f4dae45cd8d7fd51c0895a03eaa61f8abef1da3
PowreSales <ezra.seidu74@gmail.com>
2025-05-06 03:10:40 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,10 @@ self.addEventListener('fetch', event => {
   const url = event.request.url;
   if (url.includes('script.google.com')) {
     console.log('Bypassing Service Worker for GAS:', url);
-    event.respondWith(fetch(event.request));
+    event.respondWith(fetch(event.request).catch(() => {
+      console.error('Fetch failed for GAS:', url);
+      return new Response('Network error', { status: 503 });
+    }));
   } else {
     event.respondWith(fetch(event.request));
   }
